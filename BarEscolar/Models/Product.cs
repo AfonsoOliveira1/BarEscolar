52363f051b01b2d4d702d52b21adc1352246a063
Afonso Oliveira <I230065@inete.net>
2025-11-18 16:47:24 +0000

    class products fixed

--- BarEscolar/Models/Product.cs
+++ BarEscolar/Models/Product.cs
@@ -2,11 +2,13 @@
 {
     public class Product
     {
+        public string Imagepath { get; set; } = string.Empty;
         public int Id { get; set; }
-
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
+        public double Price { get;
+            set;
+        }
 
         public int CategoryId { get; set; }
 
